162595eaef1d1443b2a7fee97d9b775443529939
prudhvir3ddy <prudhvireddy.m01@gmail.com>
2025-06-05 23:53:03 +0530

    Initial Commit

---
+++ avdmanager/avdmanagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  avdmanagerApp.swift
+//  avdmanager
+//
+//  Created by Mekala Prudhvi Reddy on 05/06/25.
+//
+
+import SwiftUI
+
+@main
+struct avdmanagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ avdmanagerTests/avdmanagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  avdmanagerTests.swift
+//  avdmanagerTests
+//
+//  Created by Mekala Prudhvi Reddy on 05/06/25.
+//
+
+import Testing
+@testable import avdmanager
+
+struct avdmanagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
